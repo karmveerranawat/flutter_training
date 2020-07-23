e5339aa8679b80d96b33582bc59b9837f134eb68
karmveerranawat <karmveerranawat@gmail.com>
2020-07-23 11:32:00 +0530

    trail testing

--- lib/home_page/app_bar/appBarLayout.dart
+++ lib/home_page/app_bar/appBarLayout.dart
@@ -5,7 +5,7 @@ var appBarLogo =
     "https://instagram.fjdh1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/24838814_168164800597174_3417529666683011072_n.jpg?_nc_ht=instagram.fjdh1-1.fna.fbcdn.net&_nc_ohc=eD55qn3iBYYAX-CqO-D&oh=54dcf08f4cd71a57fafd4512a07e500f&oe=5F43808F";
 
 var titleText = Text(
-  "CyberWizard",
+  "Cyber Wizard",
   style: TextStyle(color: Colors.lightGreen),
 );
 
