eba5449ffca2cb3dbdf6380b3af8926851e874d1
karmveerranawat <karmveerranawat@gmail.com>
2020-07-31 18:56:57 +0530

    lambda function

--- lib/home_page/body/mainContainer/containerUI.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import '../mainColumn/columnUI.dart';
-import 'decoration.dart';
-
-var container = Stack(
-  children: <Widget>[containerColumn],
-);
--- lib/home_page/home_ui.dart
+++ lib/home_page/home_ui.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'app_bar/appBarLayout.dart';
-import 'body/body.dart';
+import 'package:http/http.dart' as http;
 
 mainApp() {
   FlutterStatusbarcolor.setStatusBarColor(Colors.pink);
 
   var layoutDesign = Scaffold(
     appBar: appBar,
-    body: Center(child: content),
-    //backgroundColor: Colors.grey.shade700,
+    body: RaisedButton(
+      onPressed: () async {
+        var url = "http://www.google.com";
+        var response = await http.get(url);
+        var data = response.body;
+        print(data);
+      },
+      child: Text("Scrap Data"),
+    ),
+    backgroundColor: Colors.grey.shade700,
   ); // design layout
 
   var mainContainer = MaterialApp(
